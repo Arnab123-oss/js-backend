d3813dcd79a7157d135736860c3486b21114be0c
Arnab123-oss <ajg616725@gmail.com>
2024-04-06 16:55:21 +0530

    mongo connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,27 @@
-import express from "express";
-import "dotenv/config";
-const app = express();
+import dotenv from "dotenv";
+import connectDB from "./db/database.js";
 
-// app.get("/", (req, res) => {
-//   res.send("SERVER IS READY");
-// });
- 
-
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+dotenv.config({
+  path: "./.env",
 });
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error",()=>{
+      console.log("ERROR",error);
+      throw error;
+    })
+    app.listen(process.env.PORT, () => {
+      console.log(`App listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+})();
+*/
