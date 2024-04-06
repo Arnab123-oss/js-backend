ca9a98d9dea3d75eed517df644085d39b47909a4
Arnab123-oss <ajg616725@gmail.com>
2024-04-06 19:29:51 +0530

    models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/database.js";
+import {app} from "./app.js"
 
 dotenv.config({
   path: "./.env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`App listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+  });
 
 /*
 import express from "express";
