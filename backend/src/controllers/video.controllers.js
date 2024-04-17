31b61e7939c8996310f17bd2c41c4e2a8e53cc87
Mrs Arnab Ghosh <arnab.ghosh@bassetti-group.com>
2024-04-17 19:03:29 +0530

    pusy

--- backend/src/controllers/video.controllers.js
+++ backend/src/controllers/video.controllers.js
@@ -9,7 +9,11 @@ import { uploadOnCloudinary } from "../utils/cloudinary";
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
   //TODO: get all videos based on query, sort, pagination
+  const sortTypeNum = Number(sortType) || -1;
+  const limitNum = Number(limit);
+  const pageNumber = Number(page);
 
+  await Video.createIndexes({ title: "text", description: "text" });
   if (userId && !isValidObjectId(userId)) {
     return next(new ErrorHandler(400, "Invaild User ID"));
   }
@@ -20,6 +24,57 @@ const getAllVideos = asyncHandler(async (req, res) => {
         isPublished: true,
       },
     },
-    {},
+    {
+      $addFields: {
+        sortField: {
+          $toString: "$" + (sortBy || createdAt),
+        },
+      },
+    },
+    {
+      $facet: {
+        videos: [
+          {
+            $sort: {
+              sortField: sortTypeNum,
+            },
+          },
+          {
+            $skip: (pageNumber - 1) * limitNum,
+          },
+          {
+            $limit: limitNum,
+          },
+          {
+            $Lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner_details",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    usernmae: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"owner_details"
+              }
+            }
+          }
+        ],
+        CountNumberOfVideo: [{$count:"videos"}]
+      },
+    },
+ 
   ]);
+
+
 });
